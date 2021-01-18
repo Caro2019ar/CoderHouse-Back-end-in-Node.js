3c9cfc7c92e15342bb47632d0699946ba10e3d8f
Caro2019ar <70411447+Caro2019ar@users.noreply.github.com>
2021-01-18 16:37:41 -0300

    file as an simple String

--- Clase03-Javascript Asincrono.js
+++ Clase03-Javascript Asincrono.js
@@ -1,42 +1,21 @@
 const fs = require("fs");
-var arr;
-function promiseReadFile(file) {
+const file = "Hola mundo desde archivo file";
+
+getArr(file, (resultado, tempo) => {
+	arr = resultado;
+	console.log(arr);
+	let cnt = 0;
+	id = setInterval((resultado) => {
+		if (!(cnt < arr.length - 1)) {
+			clearInterval(id);
+		}
+		console.log(arr[cnt++]);
+	}, (tempo = 1000));
+});
+
+function getArr(file, callback) {
 	return new Promise(function (resolve, reject) {
-		fs.readFile(file, function (err, data) {
-			var arr = data.toString("utf-8").split(" ");
-			if (err) throw err;
-			else {
-				resolve(file);
-				(function doTempo(tempo = 1000) {
-					let cnt = 0;
-						id = setInterval(() => {
-						if (!(cnt < arr.length - 1)) {
-							clearInterval(id);
-						}
-						console.log(arr[cnt++]);
-					}, tempo);
-				})();
-			}
-		});
+		let arr = file.split(" ");
+		return callback(arr);
 	});
 }
-
-let promR1 = promiseReadFile("./f1.txt");
-let promR2 = promiseReadFile("./f2.txt");
-let promR3 = promiseReadFile("./f3.txt");
-
-promR1
-	.then(function (file1) {
-		console.log(file1);
-		return promR2;
-	})
-	.then(function (file2) {
-		console.log(file2);
-		return promR3;
-	})
-	.then(function (file3) {
-		console.log(file3);
-	})
-	.catch(function (err) {
-		console.log("fim");
-	});
