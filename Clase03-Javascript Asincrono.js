5a7f0cd05605a3227894848e3024e04e94c6abe0
Caro2019ar <70411447+Caro2019ar@users.noreply.github.com>
2021-01-19 16:52:34 -0300

    Mensaje al final y 3 llamadas de texto funcionando

--- Clase03-Javascript Asincrono.js
+++ Clase03-Javascript Asincrono.js
@@ -1,21 +1,40 @@
-const fs = require("fs");
-const file = "Hola mundo desde archivo file";
+const file = "Hola mundo desde una constante más unas palabras termina";
+let acum;
 
-getArr(file, (resultado, tempo) => {
-	arr = resultado;
-	console.log(arr);
-	let cnt = 0;
-	id = setInterval((resultado) => {
-		if (!(cnt < arr.length - 1)) {
-			clearInterval(id);
+function getArr(file, tempo) {
+	return new Promise(function (resolve) {
+		const arr = file.split(" ");
+		let cnt = 0;
+		if (acum == undefined) {
+			acum = 0;
 		}
-		console.log(arr[cnt++]);
-	}, (tempo = 1000));
-});
-
-function getArr(file, callback) {
-	return new Promise(function (resolve, reject) {
-		let arr = file.split(" ");
-		return callback(arr);
+		id = setInterval(() => {
+			if (!(cnt < 2)) {
+				resolve(clearInterval(id));
+				//Obs> resolve() no lugar certo fez a mensagem de fim aparecer no final
+			}
+			cnt++;
+			acum = parseInt(acum);
+			console.log(arr[acum++]);
+		}, (tempo = 1000));
 	});
+	acum = acum + 1;
 }
+
+async function final() {
+	//obs> esta função final não funcionava quando estava associada a uma const
+	try {
+		console.log("1a llamada:");
+		await getArr(file, 500);
+		console.log("2a llamada:");
+		await getArr(file, 500);
+		console.log("3a llamada:");
+		await getArr(file, 500);
+
+		console.log(`Processo completo con ${acum} palabras`);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+final();
