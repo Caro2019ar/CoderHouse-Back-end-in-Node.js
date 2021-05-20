b880c9cc1b93d3625272542732a0fc9b6960c16a
Carolina <argente2019@gmail.com>
2021-05-19 22:32:47 -0300

    Clase24-Cookies

--- Clase24-Cookies/app.js
+++ Clase24-Cookies/app.js
@@ -5,49 +5,74 @@
 //  >> Ejemplos: Se adjuntan tres screenshoot con las vistas anteriormente mencionadas.
 
 // *-------------------------------------
-
+const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3000 || process.env.port;
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main.hbs" }));
+app.set("view engine", ".hbs");
+app.use(express.static("public"));
 app.use(
 	session({
 		secret: "segredo",
 		resave: false,
-		saveUnitialized: false,
-		cookie: { maxAge: 60000 },
+		saveUninitialized: false,
+		cookie: { maxAge: 1000 * 60 },
 	})
 );
 let user = "";
-// ---- Empezar con GET desde http://localhost:3000/login
-app.get("/login", (req, res, next) => {
-	// ----- Tentando com o atributo maxAge da Cookie (não deu):
-	// req.session.cookie.maxAge = 2000;
-	if (!req.query.username) {
-		res.send(
-			"Por favor, simular login con su nombre, hacer: http://localhost:3000/login?username=(su nombre)"
-		);
+let usuarios = [];
+let person = "";
+
+app.get("/", (req, res) => {
+	if (req.session.user) {
+		res.redirect("/bienvenido");
 	} else {
-		user = req.query.username;
-		req.session.user = user;
-		res.send("Bienvenido(a), " + user + " !");
-		// setTimeout(() => {
-		// 	res.redirect("/login");
-		// }, 3000);
+		res.redirect("/login");
 	}
 });
 
-//--- Para destruir la session: http://localhost:3000/logout
+app.get("/login", (req, res) => {
+	res.sendFile(__dirname + "/public/login.html");
+});
+
+// ---- Empezar desde http://localhost:3000/login
+app.post("/login", (req, res) => {
+	let { user } = req.body;
+	usuarios.push(req.body);
+	person = user;
+	console.log(`do Post ${user}`);
+	if (req.body.user) {
+		req.session.user = user;
+		res.redirect("/");
+	}
+});
+
+/* --------- Bienvenido ---------- */
+app.get("/bienvenido", (req, res) => {
+	if (req.session.user) {
+		res.render("bienvenido", {
+			bienvenido: usuarios.find((usuario) => usuario.user == req.session.user),
+		});
+	} else {
+		res.redirect("/login");
+	}
+});
+
+// //--- Para destruir la session: http://localhost:3000/logout
 app.get("/logout", (req, res) => {
-	req.session.destroy((err) => {
-		if (!err) res.send("Hasta luego " + user + " !");
-		else res.send({ status: "Logout error", body: err });
+	req.session.destroy(() => {
+		res.render("hasta", { hasta: person });
 	});
+	// setTimeout(() => {
+	// 	res.redirect("/");
+	// }, 2000);
 });
 
 app.listen(PORT, () => {
